972a49176c68b0cf4d54ceaa7fd91edebdc09c45
Mark Graybill <marktgraybill@gmail.com>
2023-08-22 15:02:47 -0400

    remove commented file diffs

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,38 +6,9 @@ import classes from './PostsList.module.css';
 
 export default function PostsList() {
   const posts = useLoaderData();
-  // const [posts, setPosts] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-
-  // useEffect(() => {
-  //   async function fetchPosts() {
-  //     setIsLoading(true);
-  //     const response = await fetch('http://localhost:8080/posts');
-  //     const resData = await response.json();
-  //     setPosts(resData.posts);
-  //     setIsLoading(false);
-  //   }
-
-  //   fetchPosts();
-  // }, []);
-
-  function addPostHandler(postData) {
-    fetch('http://localhost:8080/posts', {
-      method: 'POST',
-      body: JSON.stringify(postData),
-      headers: { 'Content-Type': 'application/json' },
-    });
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-  }
 
   return (
     <>
-      {/* {onShowModal ? (
-        <Modal onClose={onHideModal}>
-          <NewPost onCancel={onHideModal} onAddPost={addPostHandler} />
-        </Modal>
-      ) : null} */}
-      {/* {!isLoading &&  */}
       {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
@@ -45,7 +16,6 @@ export default function PostsList() {
           ))}
         </ul>
       )}
-      {/* {!isLoading &&  */}
       {posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are not posts, yet.</h2>
@@ -55,17 +25,6 @@ export default function PostsList() {
           </p>
         </div>
       )}
-      {/* {isLoading && (
-        <div
-          style={{
-            textAlign: 'center',
-            color: 'lightsalmon',
-            fontStyle: 'italic',
-          }}
-        >
-          <h3>One moment Michael, I am loading the posts now...</h3>
-        </div>
-      )} */}
     </>
   );
 }
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -5,53 +5,16 @@ import classes from './NewPost.module.css';
 import Modal from '../components/Modal';
 
 export default function NewPost() {
-  // const [enteredBody, setEnteredBody] = useState('');
-  // const [enteredAuthor, setEnteredAuthor] = useState('');
-
-  // function bodyChangeHandler(event) {
-  //   setEnteredBody(event.target.value);
-  // }
-
-  // function authorChangeHandler(event) {
-  //   setEnteredAuthor(event.target.value);
-  // }
-
-  // function onSubmitHandler(event) {
-  //   event.preventDefault();
-  //   const postData = {
-  //     body: enteredBody,
-  //     author: enteredAuthor,
-  //   };
-
-  //   onAddPost(postData);
-  //   onCancel();
-  // }
   return (
     <Modal>
-      <Form
-        method="post"
-        className={classes.form}
-        // onSubmit={onSubmitHandler}
-      >
+      <Form method="post" className={classes.form}>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea
-            id="body"
-            name="body"
-            required
-            rows={3}
-            // onChange={bodyChangeHandler}
-          />
+          <textarea id="body" name="body" required rows={3} />
         </p>
         <p>
           <label htmlFor="name">Your name</label>
-          <input
-            type="text"
-            id="name"
-            name="author"
-            required
-            // onChange={authorChangeHandler}
-          />
+          <input type="text" id="name" name="author" required />
         </p>
         <p className={classes.actions}>
           <Link to=".." type="button">
